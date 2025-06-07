7f7c66f6546d21d28e8042aef86191b4d18082fa
yash251293 <71209347+yash251293@users.noreply.github.com>
2025-06-07 22:20:59 +0530

    Update settings.py

--- cyberpanel_reseller/settings.py
+++ cyberpanel_reseller/settings.py
@@ -129,7 +129,7 @@ LOGIN_REDIRECT_URL = '/panel/profile/'
 
 # CyberPanel API Settings
 # IMPORTANT: For production, move these to environment variables or a secure vault!
-CYBERPANEL_API_URL = 'https://your_cyberpanel_server_ip:8090/api/' # e.g., 'https://<SERVER_IP>:8090/api/'
+CYBERPANEL_API_URL = 'https://5.189.179.61:8090/api/' # e.g., 'https://<SERVER_IP>:8090/api/'
 CYBERPANEL_ADMIN_USERNAME = 'admin'
 CYBERPANEL_ADMIN_PASSWORD = 'newpassword'
 
